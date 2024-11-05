418ec24d6cfd6f86dab78d01de56130c122c16fa
brunosa91 <brunosantana.engenhaira@gmail.com>
2024-11-04 22:37:58 -0300

    ajuste pom

--- api/biblioteca-etec-api/target/generated-sources/annotations/com/univesp/bibliotecaetecapi/mapper/MapperImpl.java
+++ api/biblioteca-etec-api/target/generated-sources/annotations/com/univesp/bibliotecaetecapi/mapper/MapperImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-10-19T16:40:53-0300",
+    date = "2024-11-04T22:32:17-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
